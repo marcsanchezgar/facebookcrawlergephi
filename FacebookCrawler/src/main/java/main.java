e159bdebb883f5d7dffb2efbf8d6b72b84ebb2df
marcsanchezgar <47142950+marcsanchezgar@users.noreply.github.com>
2019-06-30 15:04:30 +0200

    Add files via upload

--- FacebookCrawler/src/main/java/main.java
+++ FacebookCrawler/src/main/java/main.java
@@ -1,3 +1,7 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,6 +13,14 @@
  * @author u137679
  */
 public class main {
-    fbCrawler fbcrwl;
+
+    public static void main (String [ ] args) {
+        fbCrawler fbcrawler = new fbCrawler();
+        //mail,contrasenya
+        List<String> friends = fbcrawler.loginandCrawl("","");
+        StaticGexfGraph graph = new StaticGexfGraph();
+        graph.listToFile(friends);
+    }
+    
     
 }
