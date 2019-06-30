37129221c329dadcaab3a3d86f24cb118c2347bb
marcsanchezgar <47142950+marcsanchezgar@users.noreply.github.com>
2019-06-30 14:41:20 +0200

    Add files via upload

--- fbCrawler.java
+++ fbCrawler.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.WebElement;
  import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.interactions.internal.Coordinates;
-import org.openqa.selenium.interactions.internal.Locatable;
+import org.openqa.selenium.internal.Locatable;
 import java.util.*;
 import java.util.Arrays;
 import java.util.List;
@@ -23,8 +23,8 @@ import org.openqa.selenium.Keys;
 public class fbCrawler {
     public fbCrawler(){
     }
-    public void login(String email,String password){
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\u137679\\Desktop\\chromedriver.exe");
+    public List<String> loginandCrawl(String email,String password){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Marc\\Downloads\\facebookcrawlergephi-master\\FacebookCrawler\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-notifications");
         options.addArguments("--disable-infobars");
@@ -83,42 +83,7 @@ public class fbCrawler {
             JavascriptExecutor jse = (JavascriptExecutor)driver;
             jse.executeScript("scroll(0, 0);");
             
-            //Friends' mutual friends
-            Thread.sleep(1000);
-            int i = 0;
-            for (WebElement friend : frineds) {
-                
-                System.out.println(friend.getText());
-                /*friend.click();
-                Thread.sleep(500);
-                driver.navigate().back();
-                Thread.sleep(500);*/
-                    
-                
-                
-            }
-            int index=0;
-            /*
-            while (found1 <= 80){
-                Thread.sleep(500);
-                //scroll to the last friend found from the current loaded friend list
-                Coordinates coordinate = ((Locatable) frineds1.get(found1-1)).getCoordinates();
-                coordinate.onPage();
-                coordinate.inViewPort();
-                Thread.sleep(500);
-                frineds1 = driver.findElements(By.xpath("//*[@class='fsl fwb fcb']"));
-                found1 = frineds1.size();
-                
-
-            //driver.findElement(By.tagName("body")).sendKeys(Keys.CONTROL + Keys.HOME);
-            //
-            Jonathan, based on your original post: When you leave the page and come back, the WebElement is no longer linked. The mixed up way to accomplish what you're trying is: (1) Get the list so you know how many links 
-            you have (2) Start a loop with the count of the links (3) INSIDE the loop, get the list again (different variable) (4) Using the second variable as the link to click (5) Repeat the loop. Basically, you need to re-read 
-            in the links every time you come to the page so that they are not "stale". But you need to read the list in once so you know how many time to iterate the loop. 
             
-                                   
-            }
-        */
         }
         catch(InterruptedException e){
             System.out.println("error:" + e);
@@ -127,6 +92,7 @@ public class fbCrawler {
             System.out.println("error:" + e);
             //System.out.println("could not login in");
         }
+        return noms;
         
 }
 }
